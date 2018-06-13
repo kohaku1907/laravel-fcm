e4cbb83e0110edcf79253db3f7a12ed02d51f6d0
Tri Nguyen <nguyenmtri11@gmail.com>
2018-06-13 15:43:27 +0700

    Add config_path function

--- src/helpers.php
+++ src/helpers.php
@@ -6,3 +6,15 @@ if (!function_exists('fcm')) {
         return app('fcm');
     }
 }
+
+if (!function_exists('config_path')) {
+    /**
+     * Get the configuration path.
+     *
+     * @param  string $path
+     * @return string
+     */
+    function config_path($path = '') {
+        return app()->basePath().DIRECTORY_SEPARATOR.'config' . ($path ? DIRECTORY_SEPARATOR . $path : $path);
+    }
+}
